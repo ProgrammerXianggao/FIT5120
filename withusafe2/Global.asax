49499d3344f91f09b70e6245f7dbcf3756d93846
gx <gx@MSI>
2019-08-27 14:14:57 +1000

    Add project files.

---
+++ withusafe2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="withusafe2.MvcApplication" Language="C#" %>
---
+++ withusafe2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(withusafe2.Startup))]
+namespace withusafe2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
